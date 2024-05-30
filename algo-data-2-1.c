99cfbfae3f093211e087ef6a8433b3c6ffe18be9
korosuke <2005doi0531@gmail.com>
2024-05-30 14:38:38 +0900

    ヌル文字に変更

--- algo-data-2-1.c
+++ algo-data-2-1.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    char numstring[10000] = {/0};
+    char numstring[10000] = {'\0'};
     printf("整数を入力してください\n");
     scanf("%s",numstring);
     int sum = 0;
     int k = 0;
-    for (int i = 0; numstring[i] =! '\0'; i++)
+    for (int i = 0; numstring[i] != '\0'; i++)
     {
         k = numstring[i] - '0';
         sum = sum + k;
