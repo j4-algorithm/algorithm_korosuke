fb4eb0e226e15d480e58b38c85d2743c4749cbc1
korosuke <2005doi0531@gmail.com>
2024-07-19 11:34:10 +0900

    徳光のサイレント修正により変更

--- algo-data-4-2/algo-data-4-2.c
+++ algo-data-4-2/algo-data-4-2.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-int c = 0;
-
-int sum(int *num, int begin, int end){
-    c++;
+int sum(int *num, int begin, int end, int *counter){
+    (*counter)++;
     if(end - begin == 1){
         return num[begin];
     }else{
         int mid = (begin + end) / 2;
-        return sum(num, begin, mid) + sum(num, mid, end);
+        return sum(num, begin, mid, counter) + sum(num, mid, end, counter);
     }
 }
 
 int main(void){
     int num[1000] = {0};
     int n = 0;
+    int count = 0;
     scanf("%d", &n);
     for(int i = 0; i < n; i++){
         scanf("%d", &num[i]);
     }
-    int s = sum(num, 0, n);
+    int s = sum(num, 0, n, &count);
     printf("%d\n", s);
-    printf("%d\n", c);
+    printf("%d\n", count);
     return 0;
 }
