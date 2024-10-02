843ef4371728fb3dee213e8452802c4c415d9b86
korosuke <2005doi0531@gmail.com>
2024-10-02 09:46:45 +0900

    書き始めだけ

---
+++ algo-data-15-1/algo-data-15-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+static int *buff;
+
+static void __mergesort(int a[], int left, int right)
+{
+	if (left < right){
+		
+	}
+}
+
+int main(void)
+{
+	
+	return 0;
+}
