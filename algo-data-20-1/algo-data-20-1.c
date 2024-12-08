7427f2066060b7ccf65536b3b6068621b5814cf6
korosuke3116 <2005doi0531@gmail.com>
2024-12-08 23:36:49 +0900

    totyuu

--- algo-data-20-1/algo-data-20-1.c
+++ algo-data-20-1/algo-data-20-1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-
-void Eulergraph(int adjMatrix[size][size], int size){
-    int oredr[size];
+#define SIZE 100
+void Eulergraph(int adjMatrix[][SIZE], int size){
+    int order[size];
     int ordernums = 0;
     for(int i = 0; i < size; i++){
         order[i] = 0;
@@ -25,22 +25,79 @@ void Eulergraph(int adjMatrix[size][size], int size){
     }
 }
 
-void HamiltonianPath(int adjMatrix[size][size], int size, int start, int goal){
-    int identity matrix[size][size];
-    for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
-            identity matrix[i][j] = 0;
+
+void matrixmultiplication(int result[][SIZE], int adjMatrix[][SIZE], int size, int returnnums[][SIZE]){
+    for (int j = 0; j < size; j++){
+        for (int k = 0; k < size; k++){
+            for (int l = 0; l < size; l++){
+                returnnums[j][k] += result[j][l] * adjMatrix[l][k];
+            }
         }
     }
-    for (int i = 0;i < size; i++){
-        identity matrix[i][i] = 1;
+}
+
+void matrixaddition(int adjMatrix[][SIZE], int size, int num, int returnnums[][SIZE]){
+    int result[size][size];
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            result[i][j] = adjMatrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < num; i++){
+        matrixmultiplication(result, adjMatrix, size, returnnums);
+        for (int j = 0; j < size; j++){
+            for (int k = 0; k < size; k++){
+                result[j][k] = returnnums[j][k];
+            }
+        }
     }
     
 }
 
-void HamiltonianCycle(int adjMatrix[size][size], int size){
+void HamiltonianPath(int adjMatrix[][SIZE], int size, int start, int goal){
+    int identitymatrix[size][size];
+    int result[size][size];
+    int returnnums[size][size];
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            identitymatrix[i][j] = 0;
+        }
+    }
+    for (int i = 0;i < size; i++){
+        identitymatrix[i][i] = 1;
+    }
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            result[i][j] = identitymatrix[i][j];
+        }
+    }
+    for(int i = 0; i < size -1; i++){
+        if(result[start][goal] == 1){
+            for (int j = 0; j < size; j++){
+                for(int k = 0; k < size; k++){
+                    printf("%d ", result[j][k]);
+                }
+                printf("\n");
+            }
+            printf("可達:YES\n");
+            break;
+        }else{
+            matrixaddition(adjMatrix, size, i, returnnums);
+            for (int j = 0; j < size; j++){
+                for (int k = 0; k < size; k++){
+                    result[j][k] += returnnums[j][k];
+                }
+            }
+        }
+    }
+    printf("可達:NO\n");
+}
+
+void HamiltonianCycle(int adjMatrix[][SIZE], int size){
     int order1;
     int order2;
+    int order[size];
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
             if(adjMatrix[i][j] == 0){
@@ -75,18 +132,32 @@ int main(){
     printf("頂点の数を入力してください: ");
     scanf("%d", &size);
 
-    int adjMatrix[n][n];
+    int adjMatrix[size][size];
     printf("隣接行列を入力してください:\n");
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < size; j++) {
             scanf("%d", &adjMatrix[i][j]);
         }
     }
-    int start, goal;
-    printf("任意の2頂点を入力してください: ");
-    scanf("%d %d", &start, &goal);
-
-   
-
+    int start, goal, num;
+    printf("可達かどうかを判定しますか\nYESなら1、NOなら0を入力してください: ");
+    scanf("%d", &num);
+    if(num == 1){
+        printf("任意の2頂点を入力してください: ");
+        scanf("%d %d", &start, &goal);
+        HamiltonianPath(adjMatrix, size, start, goal);
+    }else{
+        printf("オイラーグラフかどうかを判定しますか\nYESなら1、NOなら0を入力してください: ");
+        scanf("%d", &num);
+        if(num == 1){
+            Eulergraph(adjMatrix, size);
+        }else{
+            printf("ハミルトン閉路かどうかを判定しますか\nYESなら1、NOなら0を入力してください: ");
+            scanf("%d", &num);
+            if(num == 1){
+                HamiltonianCycle(adjMatrix, size);
+            }
+        }
+    }
     return 0;
 }
