4643b511e7b90340bed6460552aa5bbac585d6ad
korosuke3116 <2005doi0531@gmail.com>
2024-12-08 20:23:28 +0900

    あと可達かどうかの判別

---
+++ algo-data-20-1/algo-data-20-1.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+
+void Eulergraph(int adjMatrix[size][size], int size){
+    int oredr[size];
+    int ordernums = 0;
+    for(int i = 0; i < size; i++){
+        order[i] = 0;
+    }
+    for(int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if(adjMatrix[i][j] == 1){
+                order[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++){
+        if(order[i] % 2 == 0){
+            ordernums++;
+        }
+    }
+    if(ordernums == size){
+        printf("オイラーグラフ:YES\n");
+    }else{
+        printf("オイラーグラフ:NO\n");
+    }
+}
+
+void HamiltonianPath(int adjMatrix[size][size], int size, int start, int goal){
+    
+}
+
+void HamiltonianCycle(int adjMatrix[size][size], int size){
+    int order1;
+    int order2;
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if(adjMatrix[i][j] == 0){
+                order1 = i;
+                order2 = j;
+                break;
+            }
+        }
+    }
+    for(int i = 0; i < size; i++){
+        order[i] = 0;
+    }
+    for(int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if(adjMatrix[i][j] == 1){
+                order[i]++;
+            }
+        }
+    }
+
+    if(order1 + order2 == size ){
+        printf("ハミルトン閉路:YES\n");
+    }else{
+        printf("ハミルトン閉路:NO\n");
+    }
+
+}
+
+
+int main(){
+    int size;
+    printf("頂点の数を入力してください: ");
+    scanf("%d", &size);
+
+    int adjMatrix[n][n];
+    printf("隣接行列を入力してください:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adjMatrix[i][j]);
+        }
+    }
+    int start, goal;
+    printf("任意の2頂点を入力してください: ");
+    scanf("%d %d", &start, &goal);
+
+   
+
+    return 0;
+}
